6b0b03341739508f9aff72b1c65844ef5902f904
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-13 16:05:07 +0000

    Changed count variable name to shape_count

--- svg_polygons.py
+++ svg_polygons.py
@@ -5,26 +5,26 @@ class Canvas:
   width = 0
   height = 0
   canvas = ''
-  count = 0
+  shape_count = 0
 
   def __init__(self, width=500, height=500):
     self.width = width
     self.height = height
 
   def polygon(self, shape, border_colour='black', fill_colour=None, opacity=1.0):
-    canvas = "\n  <g id='shape%s'>" % self.count
+    canvas = "\n  <g id='shape%s'>" % self.shape_count
     points = [(str(vertex[0]) + "," + str(vertex[1])) for vertex in shape]
     canvas += "\n    <polygon points='" + (" ".join(points)) + "' style='fill:%s; stroke:%s; fill-opacity:%s; stroke-opacity:%s; stroke-width:3; stroke-linejoin:miter;' />" % (fill_colour, border_colour, opacity, opacity)
     canvas += "\n  </g>\n"
     self.canvas += canvas
-    self.count += 1
+    self.shape_count += 1
 
   def circle(self, position, radius=1, border_colour='black', fill_colour=None, opacity=1.0):
-    canvas = "\n  <g id='shape%s'>" % self.count
+    canvas = "\n  <g id='shape%s'>" % self.shape_count
     canvas += "\n    <circle cx='%s' cy='%s' r='%s' style='stroke:%s; fill:%s; fill-opacity:%s; stroke-opacity:%s;' />" % (position[0], position[1], radius, border_colour, fill_colour, opacity, opacity)
     canvas += "\n  </g>\n"
     self.canvas += canvas
-    self.count += 1
+    self.shape_count += 1
 
   def save(self, filename='drawing'):
     canvas = self.addHeader()
@@ -37,7 +37,7 @@ class Canvas:
 
   def clear(self):
     self.canvas = ''
-    self.count = 0
+    self.shape_count = 0
 
   def addHeader(self):
     return "<svg xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#' xmlns:svg='http://www.w3.org/2000/svg' xmlns='http://www.w3.org/2000/svg' version='1.1' width='" + str(self.width) + "' height='" + str(self.height) + "'>\n"
