632d01f8cd998019de2d0fd80f641fc5aeeac430
Timo <timo.juez@gmail.com>
2017-04-22 14:36:25 +0300

    unicode and utf-8 instead of str

--- svg_polygons.py
+++ svg_polygons.py
@@ -4,8 +4,8 @@ class Canvas:
 
   width = 0
   height = 0
-  canvas = ''
-  captions = ''
+  canvas = u''
+  captions = u''
   shape_count = 0
 
   def __init__(self, width=500, height=500):
@@ -13,22 +13,22 @@ class Canvas:
     self.height = height
 
   def polygon(self, shape, border_colour='black', fill_colour=None, opacity=1.0, border_width=3, caption=None, caption_size=35):
-    canvas = "\n  <g id='shape%s'>" % self.shape_count
-    points = [(str(vertex[0]) + "," + str(vertex[1])) for vertex in shape]
-    canvas += "\n    <polygon points='" + (" ".join(points)) + "' style='fill:%s; stroke:%s; fill-opacity:%s; stroke-opacity:%s; stroke-width:%s; stroke-linejoin:miter;' />" % (fill_colour, border_colour, opacity, opacity, border_width)
+    canvas = u"\n  <g id='shape%s'>" % self.shape_count
+    points = [(unicode(vertex[0]) + "," + unicode(vertex[1])) for vertex in shape]
+    canvas += u"\n    <polygon points='" + (" ".join(points)) + u"' style='fill:%s; stroke:%s; fill-opacity:%s; stroke-opacity:%s; stroke-width:%s; stroke-linejoin:miter;' />" % (fill_colour, border_colour, opacity, opacity, border_width)
     if caption:
         X = [x for x,y in shape]
         Y = [y for x,y in shape]
         centre = (min(X)+(max(X)-min(X))/2, min(Y)+(max(Y)-min(Y))/2)
-        self.captions += '\n  <text x="%d" y="%d" font-family="Verdana" font-size="%d" fill="black">%s</text>'%(centre[0],centre[1],caption_size,caption)
-    canvas += "\n  </g>\n"
+        self.captions += u'\n  <text x="%d" y="%d" font-family="Verdana" font-size="%d" fill="black">%s</text>'%(centre[0],centre[1],caption_size,caption)
+    canvas += u"\n  </g>\n"
     self.canvas += canvas
     self.shape_count += 1
 
   def circle(self, position, radius=1, border_colour='black', fill_colour=None, opacity=1.0):
-    canvas = "\n  <g id='shape%s'>" % self.shape_count
-    canvas += "\n    <circle cx='%s' cy='%s' r='%s' style='stroke:%s; fill:%s; fill-opacity:%s; stroke-opacity:%s;' />" % (position[0], position[1], radius, border_colour, fill_colour, opacity, opacity)
-    canvas += "\n  </g>\n"
+    canvas = u"\n  <g id='shape%s'>" % self.shape_count
+    canvas += u"\n    <circle cx='%s' cy='%s' r='%s' style='stroke:%s; fill:%s; fill-opacity:%s; stroke-opacity:%s;' />" % (position[0], position[1], radius, border_colour, fill_colour, opacity, opacity)
+    canvas += u"\n  </g>\n"
     self.canvas += canvas
     self.shape_count += 1
 
@@ -37,19 +37,19 @@ class Canvas:
     canvas += self.addCanvas()
     canvas += self.addFooter()
     f = open(filename + '.svg', 'w')
-    f.write(canvas)
+    f.write(canvas.encode("utf-8"))
     f.close()
     print "File saved as %s.svg" % filename
 
   def clear(self):
-    self.canvas = ''
+    self.canvas = u''
     self.shape_count = 0
 
   def addHeader(self):
-    return "<svg xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#' xmlns:svg='http://www.w3.org/2000/svg' xmlns='http://www.w3.org/2000/svg' version='1.1' width='" + str(self.width) + "' height='" + str(self.height) + "'>\n"
+    return u"<svg xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#' xmlns:svg='http://www.w3.org/2000/svg' xmlns='http://www.w3.org/2000/svg' version='1.1' width='" + unicode(self.width) + u"' height='" + unicode(self.height) + "'>\n"
 
   def addCanvas(self):
-    return self.canvas + "\n"
+    return self.canvas + u"\n"
 
   def addFooter(self):
-    return "%s\n</svg>"%self.captions
+    return u"%s\n</svg>"%self.captions
